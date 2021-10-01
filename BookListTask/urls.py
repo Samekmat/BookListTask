74640e8522061422943f20355b2c5c3130ccb3e6
Mateusz Samek <samekmat@gmail.com>
2021-10-01 20:44:05 +0200

    Handle add-book

--- BookListTask/urls.py
+++ BookListTask/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from base.views import index
+from base.views import index, add_book
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
+    path('add-book/', add_book, name='add-book'),
 ]
--- base/views.py
+++ base/views.py
@@ -1,6 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import BookForm
 
 
 def index(request):
-    return render(request, 'base.html')
+    return render(request, 'index.html')
 
+
+def add_book(request):
+    form = BookForm()
+    if request.method == 'POST':
+        form = BookForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+    ctx = {'form': form}
+    return render(request, 'book_form.html', ctx)
