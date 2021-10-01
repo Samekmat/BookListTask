1df2b9ec31cc3dfd457a892c378df59857fb6fbb
Mateusz Samek <samekmat@gmail.com>
2021-10-01 23:52:38 +0200

    Add book list and single book url

--- BookListTask/urls.py
+++ BookListTask/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from base.views import index, add_book
+from base.views import index, add_book, book_list, book_page
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
     path('add-book/', add_book, name='add-book'),
+    path('books/', book_list, name='book-list'),
+    path('book/<str:pk>/', book_page, name='book_page')
 ]
