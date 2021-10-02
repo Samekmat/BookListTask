53471fea315689ff85b4062cda36105bbf31f636
Mateusz Samek <samekmat@gmail.com>
2021-10-02 04:04:25 +0200

    Add styling

--- base/forms.py
+++ base/forms.py
@@ -1,8 +1,20 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, TextInput
 from .models import Book
+
+
+STYLE = {'class': 'input is-dark is-rounded is-large is-info'}
 
 
 class BookForm(ModelForm):
     class Meta:
         model = Book
         fields = '__all__'
+        widgets = {
+            'title': TextInput(attrs=STYLE),
+            'author': TextInput(attrs=STYLE),
+            'publish_date': TextInput(attrs=STYLE),
+            'ISBN': TextInput(attrs=STYLE),
+            'num_of_pages': TextInput(attrs=STYLE),
+            'cover_link': TextInput(attrs=STYLE),
+            'publish_lang': TextInput(attrs=STYLE)
+        }
