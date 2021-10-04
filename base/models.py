587d40e6434e0e58e0920c44b8fb27eb4141bc28
Mateusz Samek <samekmat@gmail.com>
2021-10-05 00:48:14 +0200

    Fix multiple codes error

--- base/models.py
+++ base/models.py
@@ -5,7 +5,7 @@ class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=120)
     publish_date = models.CharField(max_length=30)
-    ISBN = models.CharField(max_length=13)
+    ISBN = models.CharField(max_length=100)
     num_of_pages = models.IntegerField(null=True)
     cover_link = models.URLField(default='', blank=True)
     publish_lang = models.CharField(max_length=100)
