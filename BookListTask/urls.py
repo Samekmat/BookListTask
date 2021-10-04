db4f0478ed05f5acb0adc3a414f743a5b80a55df
Mateusz Samek <samekmat@gmail.com>
2021-10-05 00:25:07 +0200

    Restore some changes

--- BookListTask/urls.py
+++ BookListTask/urls.py
@@ -4,7 +4,7 @@ from base.views import index, add_book, book_list, book_page, update_book, delet
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('/', index, name='index'),
+    path('', index, name='index'),
     path('books/', book_list, name='book-list'),
     path('book/<int:pk>/', book_page, name='book_page'),
     path('add-book/', add_book, name='add-book'),
