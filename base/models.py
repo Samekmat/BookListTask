08f983236c5e5c2965e275c6646a6f221e21867a
Mateusz Samek <samekmat@gmail.com>
2021-11-02 15:05:14 +0100

    Alter isbn table 'null=true'

--- base/models.py
+++ base/models.py
@@ -5,7 +5,7 @@ class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=120)
     publish_date = models.CharField(max_length=30)
-    ISBN = models.CharField(max_length=200)
+    ISBN = models.CharField(max_length=200, null=True)
     num_of_pages = models.IntegerField(null=True)
     cover_link = models.URLField(default='', blank=True)
     publish_lang = models.CharField(max_length=100)
